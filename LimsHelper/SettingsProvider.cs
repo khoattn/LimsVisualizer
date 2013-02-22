06ceb6f363443912293eac1484c627f8aabc2597
badlymad <stefan.raminger@gmail.com>
2013-02-22 11:26:23 +0100

    Major changes in SettingsProvider
    Fixed some major bugs
    Changed DueTime from type int to TimeSpan
    Moved LimsVisualizerSettings and LimsSimulatorSettings classes back to
    SettingsProvider file

--- LimsHelper/SettingsProvider.cs
+++ LimsHelper/SettingsProvider.cs
@@ -4,13 +4,8 @@ using System.IO;
 
 namespace LimsHelper
 {
-    class SettingsProvider
+    public class SettingsProvider
     {
-        private string SettingsFile
-        {
-            get { return Path.Combine(Environment.GetEnvironmentVariable("TEMP"), ApplicationName, @"settings.txt"); }
-        }
-
         public string ApplicationName { private get; set; }
         public Logger LogWriter { private get; set; }
 
@@ -19,21 +14,22 @@ namespace LimsHelper
             try
             {
                 limsVisualizerSettings = new LimsVisualizerSettings();
+                var settingsFile = _GetSettingsFilePath();
 
-                LogWriter.WriteDebugMessage(string.Format("Reading settings. File: '{0}'", SettingsFile));
-                if (!File.Exists(SettingsFile))
+                LogWriter.WriteDebugMessage(string.Format("Reading settings. File: '{0}'", settingsFile));
+                if (!File.Exists(settingsFile))
                 {
                     limsVisualizerSettings.FilePath = Environment.GetEnvironmentVariable("SYSTEMDRIVE") + @"\Anton Paar\Davis 5\Data Monitoring\";
-                    limsVisualizerSettings.DueTime = 500;
+                    limsVisualizerSettings.DueTime = new TimeSpan(0, 0, 0, 0, 500);
                     LogWriter.WriteDebugMessage("Settings file not found, using default settings.");
                     return;
                 }
 
-                var fileStream = new FileStream(SettingsFile, FileMode.Open, FileAccess.Read, FileShare.None);
+                var fileStream = new FileStream(settingsFile, FileMode.Open, FileAccess.Read, FileShare.None);
                 var settingsReader = new StreamReader(fileStream);
 
                 limsVisualizerSettings.FilePath = settingsReader.ReadLine();
-                limsVisualizerSettings.DueTime = Convert.ToInt16(settingsReader.ReadLine());
+                limsVisualizerSettings.DueTime = new TimeSpan(0, 0, 0, 0, Convert.ToInt16(settingsReader.ReadLine()));
                 settingsReader.Close();
                 LogWriter.WriteDebugMessage(string.Format("Settings file found. FilePath: '{0}' DueTime: '{1}'",
                     limsVisualizerSettings.FilePath,
@@ -52,23 +48,24 @@ namespace LimsHelper
             try
             {
                 limsSimulatorSettings = new LimsSimulatorSettings();
+                var settingsFile = _GetSettingsFilePath();
 
-                LogWriter.WriteDebugMessage(string.Format("Reading settings. File: '{0}'", SettingsFile));
-                if (!File.Exists(SettingsFile))
+                LogWriter.WriteDebugMessage(string.Format("Reading settings. File: '{0}'", settingsFile));
+                if (!File.Exists(settingsFile))
                 {
                     limsSimulatorSettings.SampleFile = string.Empty;
                     limsSimulatorSettings.DestinationPath = Environment.GetEnvironmentVariable("SYSTEMDRIVE") + @"\Anton Paar\Davis 5\Data Monitoring\";
-                    limsSimulatorSettings.DueTime = 500;
+                    limsSimulatorSettings.DueTime = new TimeSpan(0, 0, 0, 1);
                     LogWriter.WriteDebugMessage("Settings file not found, using default settings.");
                     return;
                 }
 
-                var fileStream = new FileStream(SettingsFile, FileMode.Open, FileAccess.Read, FileShare.None);
+                var fileStream = new FileStream(settingsFile, FileMode.Open, FileAccess.Read, FileShare.None);
                 var settingsReader = new StreamReader(fileStream);
 
                 limsSimulatorSettings.SampleFile = settingsReader.ReadLine();
                 limsSimulatorSettings.DestinationPath = settingsReader.ReadLine();
-                limsSimulatorSettings.DueTime = Convert.ToInt16(settingsReader.ReadLine());
+                limsSimulatorSettings.DueTime = new TimeSpan(0, 0, Convert.ToInt16(settingsReader.ReadLine()));
                 settingsReader.Close();
                 LogWriter.WriteDebugMessage(string.Format("Settings file found. SampleFile: '{0}' DestinationPath: '{1}' DueTime: '{2}'",
                         limsSimulatorSettings.SampleFile,
@@ -87,14 +84,16 @@ namespace LimsHelper
         {
             try
             {
+                var settingsFile = _GetSettingsFilePath();
+
                 LogWriter.WriteDebugMessage(string.Format("Writing settings file. FilePath: '{0}' DueTime: '{1}'",
                     limsVisualizerSettings.FilePath,
                     limsVisualizerSettings.DueTime));
-                var fileStream = new FileStream(SettingsFile, FileMode.Create, FileAccess.Write, FileShare.None);
+                var fileStream = new FileStream(settingsFile, FileMode.Create, FileAccess.Write, FileShare.None);
                 var settingsWriter = new StreamWriter(fileStream);
 
                 settingsWriter.WriteLine(limsVisualizerSettings.FilePath);
-                settingsWriter.WriteLine(limsVisualizerSettings.DueTime.ToString(CultureInfo.InvariantCulture));
+                settingsWriter.WriteLine(limsVisualizerSettings.DueTime.TotalMilliseconds);
                 settingsWriter.Close();
                 LogWriter.WriteDebugMessage("Settings file was successfully written.");
             }
@@ -110,17 +109,19 @@ namespace LimsHelper
         {
             try
             {
+                var settingsFile = _GetSettingsFilePath();
+
                 LogWriter.WriteDebugMessage(
                     string.Format("Writing settings file. SampleFile: '{0}' DestinationPath: '{1}' DueTime: '{2}'",
                         limsSimulatorSettings.SampleFile,
                         limsSimulatorSettings.DestinationPath,
                         limsSimulatorSettings.DueTime));
-                var fileStream = new FileStream(SettingsFile, FileMode.Create, FileAccess.Write, FileShare.None);
+                var fileStream = new FileStream(settingsFile, FileMode.Create, FileAccess.Write, FileShare.None);
                 var settingsWriter = new StreamWriter(fileStream);
 
                 settingsWriter.WriteLine(limsSimulatorSettings.SampleFile);
                 settingsWriter.WriteLine(limsSimulatorSettings.DestinationPath);
-                settingsWriter.WriteLine(limsSimulatorSettings.DueTime.ToString(CultureInfo.InvariantCulture));
+                settingsWriter.WriteLine(limsSimulatorSettings.DueTime.TotalSeconds);
                 settingsWriter.Close();
                 LogWriter.WriteDebugMessage("Settings file was successfully written.");
             }
@@ -131,7 +132,23 @@ namespace LimsHelper
                 throw;
             }
         }
+
+        private string _GetSettingsFilePath()
+        {
+            return Path.Combine(Environment.GetEnvironmentVariable("TEMP"), ApplicationName, @"settings.txt");
+        }
     }
 
+    public class LimsVisualizerSettings
+    {
+        public string FilePath { get; set; }
+        public TimeSpan DueTime { get; set; }
+    }
 
+    public class LimsSimulatorSettings
+    {
+        public string SampleFile { get; set; }
+        public string DestinationPath { get; set; }
+        public TimeSpan DueTime { get; set; }
+    }
 }
